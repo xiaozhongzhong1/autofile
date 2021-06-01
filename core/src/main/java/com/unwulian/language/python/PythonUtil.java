a2fd68f1df3b1b1762ffc920863b227f6e0ffa3c
wuzhongliang <wuzhongliang@unwulian.com>
2021-06-01 13:42:35 +0800

    新建项目

--- core/src/main/java/com/unwulian/language/python/PythonUtil.java
+++ core/src/main/java/com/unwulian/language/python/PythonUtil.java
@@ -4,33 +4,42 @@ import cn.hutool.core.io.IoUtil;
 import com.google.common.base.Joiner;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.net.URL;
 
 /**
  * python调用工具
  */
 public class PythonUtil {
+    private static final String PY_ENV = "D:\\software\\python\\python";
 
-    public static String invoke(String pyFile, String... params) {
+    public static String invoke(String pyEnv, String pyFile, String... params) {
         String content;
         try {
             String python = Joiner.on(" ")
                     .skipNulls()
-                    .join("python", pyFile, params);
+                    .join(pyEnv, pyFile, params);
 
             Process pr = Runtime.getRuntime().exec(python);
-            BufferedReader reader = IoUtil.getReader(pr.getInputStream(), "utf-8");
+            int i = pr.waitFor();
+            if (i != 0) {
+                throw new Exception("py run error");
+            }
+            BufferedReader reader = IoUtil.getReader(pr.getInputStream(), "GBK");
             content = IoUtil.read(reader);
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return content;
     }
 
+    public static String invokeDefault(String pyFile, String... params) {
+        String invoke = invoke(PY_ENV, pyFile, params);
+        return invoke;
+    }
+
     public static void main(String[] args) {
         URL resource = PythonUtil.class.getClassLoader().getResource("py/extractTableFromPDF.py");
-        String invoke = PythonUtil.invoke(resource.getFile(), "C:\\Users\\Administrator\\Desktop\\test.pdf", "150");
+        String invoke = PythonUtil.invokeDefault(resource.getPath().substring(1), "C:\\Users\\Administrator\\Desktop\\test.pdf", "150");
         System.out.println(invoke);
     }
 }
--- core/src/main/resources/py/extractTableFromPDF.py
+++ core/src/main/resources/py/extractTableFromPDF.py
@@ -1,13 +1,16 @@
+
+
 import pdfplumber
 import sys
 
 def extract(path,pageNo):
     with pdfplumber.open(path) as pdf:
-    page = pdf.pages[pageNo]
-    table2 = page01.extract_tables()
-    return table2
+        page01 = pdf.pages[pageNo]
+        table2 = page01.extract_tables()
+        return table2
 
 if __name__ == "__main__":
     path = sys.argv[1]
     pageNo = sys.argv[2]
-    return extract(path,pageNo)
+    content = extract(path,int(pageNo))
+    print(content)
