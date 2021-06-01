15913c96101577284b6bcd71afbfbe75e07dd380
wuzhongliang <wuzhongliang@unwulian.com>
2021-06-01 16:26:48 +0800

    新建项目

--- api/src/main/java/com/example/api/service/impl/FaceRecognitionServiceImpl.java
+++ api/src/main/java/com/example/api/service/impl/FaceRecognitionServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.api.service.impl;
 
+import cn.hutool.core.io.FileUtil;
 import com.example.api.service.IFaceRecognitionService;
 import com.unwulian.doc.pdf.PDFReader;
 import com.unwulian.language.python.PythonUtil;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
@@ -13,23 +15,30 @@ import java.util.Objects;
 @Service
 public class FaceRecognitionServiceImpl implements IFaceRecognitionService {
     private PDFReader pdfReader = new PDFReader();
-
+    private final String DIR = "C:\\Users\\Administrator\\Desktop\\copy";
+    private final String CONTENT_PATH =  DIR.concat("\\content.txt");
+    private final String TABLES_PATH =  DIR.concat("\\tables.txt");
     /**
      * 通过规则去提取
      */
     @Override
     public void extract() {
+
+
         PDDocument doc = null;
         try {
             String file = "C:\\Users\\Administrator\\Desktop\\test.pdf";
             doc = pdfReader.read(file);
-            String content = pdfReader.getContent(doc, 8, 9);
+            String content = pdfReader.getContent(doc, 8, 151);
+            FileUtil.writeString(content,new File(CONTENT_PATH),"UTF-8");
+            System.out.println("content is ok");
+
+
             URL resource = PythonUtil.class.getClassLoader().getResource("py/extractTableFromPDF.py");
             String tables = PythonUtil.invokeDefault(resource.getPath().substring(1), "C:\\Users\\Administrator\\Desktop\\test.pdf"
-                    , "8", "9");
-
-            System.out.println(tables);
-
+                    , "8", "10");
+            FileUtil.writeString(tables,new File(TABLES_PATH),"UTF-8");
+            System.out.println("tables is ok");
 
         } catch (Exception e) {
             System.out.println(e);
